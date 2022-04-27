18137a2445902fb5ca95efce4de9e31d7a5d644a
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-04-27 11:01:09 -0700

    Create rook.sv

---
+++ rook.sv
@@ -0,0 +1,11 @@
+/*
+Encoding:
+color: black = 1, white = 0
+position (0,0) is in top left of board
+position (7,7) is in bottom right of board
+rook allowable moves: 
+rookAllowUp[2:0]
+rookAllowRight[2:0]
+rookAllowDown[2:0]
+rookAllowLeft[2:0]
+*/
