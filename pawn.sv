2d096aca9b75bd4a1c8db4f48b2a69f7edcee5a6
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-04-28 18:39:02 -0700

    Update pawn.sv

--- pawn.sv
+++ pawn.sv
@@ -19,33 +19,36 @@ module pawn(input  logic [2:0] row,
 						input  logic [2:0] boardPos [7:0][7:0],
 						output logic [2:0] pawnAllow);
 
-//put boardpos checks in else if
 	always_comb
-		case(color)
-			//white pieces
-			1'b0:	begin
-				//determines if possible movements are allowed based on position and other pieces
-				if((row == 3'b0) | (boardPos[row-1][column][0] == 1'b1))																	pawnAllow[2] = 1'b0;		// if in top row, or there is a piece in row above of pawn, cannot move forward
-				else																														pawnAllow[2] = 1'b1;		// can move forward
-				if((row == 3'b0) | (column == 3'b0) | (boardPos[row-1][column-1][0] == 1'b0) | (boardPos[row-1][column-1][1:0] == 2'b01))	pawnAllow[1] = 1'b0;		// if in top row, or in leftmost column, or if there is no piece diagonally to the left, or if there is a white piece diagonally to the left, cannot move diagonally left
-				else																														pawnAllow[1] = 1'b1;		// can move diagleft
-				if((row == 3'b0) | (column == 3'b111) | (boardPos[row-1][column+1][0] == 1'b0) | (boardPos[row-1][column+1][1:0] == 2'b01))	pawnAllow[0] = 1'b0;		// if in top row, or in rightmost column, or if there is no piece diagonally to the right, or if there is a white piece diagonally to the right, cannot move diagonally right
-				else																														pawnAllow[0] = 1'b1;		// can move diagright
+		case(color)			//determines if possible movements are allowed based on position and other pieces
+			1'b0:	begin		//white pieces
+				if((row == 3'b0))																																						pawnAllow[2] = 1'b0;	// if in top row cannot move forward
+				else if(boardPos[row-1][column][0] == 1'b1)																									pawnAllow[2] = 1'b0;	// if there is a piece in row above of pawn, cannot move forward
+				else																																												pawnAllow[2] = 1'b1;	// can move forward
+				
+				if((row == 3'b0) | (column == 3'b0))																												pawnAllow[1] = 1'b0;	// if in top row, or in leftmost column cannot move diagonally left
+				else if((boardPos[row-1][column-1][0] == 1'b0) | (boardPos[row-1][column-1][1:0] == 2'b01))	pawnAllow[1] = 1'b0;	// if there is no piece diagonally to the left, or if there is a white piece diagonally to the left, cannot move diagonally left
+				else																																												pawnAllow[1] = 1'b1;	// can move diagleft
+				
+				if((row == 3'b0) | (column == 3'b111))																											pawnAllow[0] = 1'b0;	// if in top row, or in rightmost column cannot move diagonally right
+				else if((boardPos[row-1][column+1][0] == 1'b0) | (boardPos[row-1][column+1][1:0] == 2'b01))	pawnAllow[0] = 1'b0;	// if there is no piece diagonally to the right, or if there is a white piece diagonally to the right, cannot move diagonally right
+				else																																												pawnAllow[0] = 1'b1;	// can move diagright
 			end
 			//black pieces
 			1'b1:	begin
 				//determines if possible movements are allowed based on position and other pieces
-				if((row == 3'b111) | (boardPos[row+1][column][0] == 1'b1))																		pawnAllow[2] = 1'b0;	// if in bottom row, or there is a piece in row below of pawn, cannot move forward
-				else																															pawnAllow[2] = 1'b1;	// can move forward
-				if((row == 3'b111) | (column == 3'b0) | (boardPos[row+1][column-1][0] == 1'b0) | (boardPos[row+1][column-1][1:0] == 2'b11))		pawnAllow[1] = 1'b0;	// if in bottom row, or in leftmost column, or if there is no piece diagonally to the left, or if there is a black piece diagonally to the left, cannot move diagonally left
-				else																															pawnAllow[1] = 1'b1;	// can move diagleft
-				if((row == 3'b111) | (column == 3'b111) | (boardPos[row+1][column+1][0] == 1'b0) | (boardPos[row+1][column+1][1:0] == 2'b11))	pawnAllow[0] = 1'b0;	// if in bottom row, or in rightmost column, or if there is no piece diagonally to the right, or if there is a black piece diagonally to the right, cannot move diagonally right
-				else																															pawnAllow[0] = 1'b1;	// can move diagright
+				if(row == 3'b111)																																						pawnAllow[2] = 1'b0;	// if in bottom row cannot move forward
+				else if(boardPos[row+1][column][0] == 1'b1)																									pawnAllow[2] = 1'b0;	// if there is a piece in row below of pawn, cannot move forward
+				else																																												pawnAllow[2] = 1'b1;	// can move forward
+				
+				if((row == 3'b111) | (column == 3'b0))																											pawnAllow[1] = 1'b0;	// if in bottom row, or in leftmost column cannot move diagonally left
+				else if((boardPos[row+1][column-1][0] == 1'b0) | (boardPos[row+1][column-1][1:0] == 2'b11))	pawnAllow[1] = 1'b0;	// if there is no piece diagonally to the left, or if there is a black piece diagonally to the left, cannot move diagonally left
+				else																																												pawnAllow[1] = 1'b1;	// can move diagleft
+				
+				if((row == 3'b111) | (column == 3'b111))																										pawnAllow[0] = 1'b0;	// if in bottom row, or in rightmost column cannot move diagonally right
+				else if((boardPos[row+1][column+1][0] == 1'b0) | (boardPos[row+1][column+1][1:0] == 2'b11))	pawnAllow[0] = 1'b0; 	// if there is no piece diagonally to the right, or if there is a black piece diagonally to the right, cannot move diagonally right
+				else																																												pawnAllow[0] = 1'b1;	// can move diagright
 			end
 		endcase
 	
-
 endmodule
-
-
-
