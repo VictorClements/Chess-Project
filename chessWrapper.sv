a2765e49369540ac7fd1ba2e86e7986066e0ff50
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-05-05 13:46:27 -0700

    Update chessWrapper.sv

--- chessWrapper.sv
+++ chessWrapper.sv
@@ -1,6 +1,6 @@
 module chessWrapper(input  logic	CLOCK_50,       // FPGA 50MHz Clock for VGA
-		    input  logic [0:0]	SW,             // for reset
-                    input  logic [3:0]	KEY,            // for up/down/left/right
+		    input  logic [17:0]	SW,             // for reset and UP (17 and 0)
+		    input  logic [2:0]	KEY,            // for positions and select
                     output logic [6:0]	HEX1, HEX0,     // display of current cursor position with sevenseg display
                     output logic	VGA_CLK,        // clock for VGA to run on
                     output logic	VGA_HS,         // horizontal sync
@@ -11,6 +11,6 @@ module chessWrapper(input  logic	CLOCK_50,       // FPGA 50MHz Clock for VGA
                     output logic [7:0]	VGA_G,          // green pixel outputs
                     output logic [7:0]	VGA_B);         // blue pixel outputs
         
-chess chess(CLOCK_50, SW[0], KEY[3:0], HEX1, HEX0, VGA_CLK, VGA_HS, VGA_VS, VGA_SYNC_N, VGA_BLANK_N, VGA_R, VGA_G, VGA_B);
+	chess chess(CLOCK_50, SW[17], KEY[1], KEY[0], SW[0], KEY[2], HEX1, HEX0, VGA_CLK, VGA_HS, VGA_VS, VGA_SYNC_N, VGA_BLANK_N, VGA_R, VGA_G, VGA_B);
 
 endmodule
