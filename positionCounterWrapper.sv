d9c2a14587d9060e31625dfcb95bed0ddc5d9457
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-05-05 13:32:09 -0700

    Update positionCounterWrapper.sv

--- positionCounterWrapper.sv
+++ positionCounterWrapper.sv
@@ -1,9 +1,9 @@
 module positionCounterWrapper(input  logic [1:0] KEY,
 							  input  logic [17:0] SW, //[17,0]
-							  output logic [7:0] LEDR,
+							  output logic [5:0] LEDR,
 							  output logic [6:0] HEX0, HEX1);
 					   
 	positionCounter myPosition(	KEY[1], KEY[0], SW[17], SW[0],
-								LEDR[7:4], LEDR[3:0], HEX1[6:0], HEX0[6:0]);
+								LEDR[5:3], LEDR[2:0], HEX1[6:0], HEX0[6:0]);
 
 endmodule
