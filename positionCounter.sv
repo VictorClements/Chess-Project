eb54a92b263ff5a7a29eda924da4c5612b957b1a
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-05-05 13:16:39 -0700

    Update positionCounter.sv

--- positionCounter.sv
+++ positionCounter.sv
@@ -1,9 +1,10 @@
 module positionCounter(input  logic 	  clk1, clk2,
 					   input  logic 	  reset,
 					   input  logic 	  UP,
+					   output logic	[3:0] rowNum, columnNum,
 					   output logic [6:0] rowDisplay, columnDisplay);
 
-	Counter4    row(clk1, reset, UP, rowDisplay);
-	Counter4 column(clk2, reset, UP, columnDisplay);
+	Counter4    row(clk1, reset, UP, rowNum, rowDisplay);
+	Counter4 column(clk2, reset, UP, columnNum, columnDisplay);
 	
 endmodule
