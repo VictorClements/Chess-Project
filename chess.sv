3c8d8a04bfe586b8ee7beb93b5420964b8b4a40f
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-05-05 13:40:39 -0700

    Update chess.sv

--- chess.sv
+++ chess.sv
@@ -69,24 +69,23 @@ always_ff @(posedge reset)	begin
 */
 
 module chess(input  logic	clk, reset,	//clk and reset for system 
-	     input  logic 	rowChange, columnChange,
-	     input  logic	UP,
-	     
-	     output logic [6:0]	rowDisplay, columnDisplay,
-	     output logic	vgaclk,
-	     output logic	hsync, vsync,
-	     output logic	sync_b, blank_b
-	     output logic [7:0]	r, g, b);
+	     input  logic 	rowChange, columnChange, //inputs from board from key buttons
+	     input  logic	UP,				//input from board from lever
+	     output logic [6:0]	rowDisplay, columnDisplay,	//output cursor position onto hex sevensegs
+	     output logic	vgaclk,				//vga output clk
+	     output logic	hsync, vsync,			//vga output horiz and vert sync
+	     output logic	sync_b, blank_b,		//vga outupt ?
+	     output logic [7:0]	r, g, b);			//vga pixel color
 
 // boardPos is a 2-D array with 8 rows and 8 columns, and each element of the array is a 5 bit value
 // bit 0 represents if the space is empty (no piece on the space) space is occuped = 1 and space is empty = 0
 // bit 1 represents the color of the piece on the space, no piece = 0 (default),  white = 0, black = 1
 // bits 4:2 represents piece type 001 Pawn, 010 knight, 011 bishop, 100 rook, 101 queen, 110 king, 000 N/A	
-	
+  logic [2:0] rowNum, columnNum;
   logic [4:0] boardPos [7:0][7:0];
   initial $readmemb("start.txt", boardPos);
 	
-  positionCounter myPos(rowChange, columnChange, reset, UP, rowDisplay, columnDisplay);
+  positionCounter myPos(rowChange, columnChange, reset, UP, rowNum, columnNum, rowDisplay, columnDisplay);
 
 endmodule
 
